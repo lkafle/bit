06ff9c0ad30baa79e1743f03cf070c6e7e9bf463
Uri Kutner <urikutner@gmail.com>
2021-02-15 15:39:17 +0200

    use error boundary (un-styled)

--- scopes/compositions/ui/composition-card/composition-card.tsx
+++ scopes/compositions/ui/composition-card/composition-card.tsx
@@ -1,5 +1,6 @@
 import React, { useMemo } from 'react';
 import classNames from 'classnames';
+import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
 import { humanizeCompositionId } from '@teambit/model.composition-id';
 import { CompositionType } from '@teambit/model.composition-type';
 import { Card } from '@teambit/base-ui.surfaces.card';
@@ -19,10 +20,35 @@ export function CompositionCard({ Composition, name }: CompositionCardProps) {
 
   return (
     <Card elevation="low" className={classNames(styles.compositionCard)}>
-      <div style={canvas} className={styles.compositionContainer}>
-        <Composition />
-      </div>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <div style={canvas} className={styles.compositionContainer}>
+          <Composition />
+        </div>
+      </ErrorBoundary>
       <div className={classNames(styles.title, colorPalette.emphasized, themedText)}>{humanizedName}</div>
     </Card>
+  );
+}
+
+function ErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
+  console.error(error);
+
+  return (
+    <div
+      style={{
+        height: '100%',
+        border: '4px solid #f086a0',
+        borderRadius: '4px 4px 0 0',
+        textAlign: 'center',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      Failed to render 😨
+      <br />
+      <button onClick={resetErrorBoundary}>retry</button>
+    </div>
   );
 }
